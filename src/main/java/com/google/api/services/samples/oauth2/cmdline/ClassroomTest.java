edfc02ee3e0c40776ed32861258fa72799208e01
Rohit <ubigrade@gmail.com>
2019-10-20 23:03:00 +0200

    classroom try .

---
+++ src/main/java/com/google/api/services/samples/oauth2/cmdline/ClassroomTest.java
@@ -0,0 +1,8 @@
+package com.google.api.services.samples.oauth2.cmdline;
+
+public class ClassroomTest {
+    public static void Main(String[] args)
+    {
+        System.out.println("ClassroomTest");
+    }
+}
